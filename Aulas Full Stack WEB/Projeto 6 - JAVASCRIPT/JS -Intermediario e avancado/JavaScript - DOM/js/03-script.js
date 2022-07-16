cb05a00e9859643a23f51cc05ded295d94e8afae
LEANDRO CF <83041805+leandrocf79@users.noreply.github.com>
2022-07-15 21:37:56 -0300

    Add files via upload

---
+++ Aulas Full Stack WEB/Projeto 6 - JAVASCRIPT/JS -Intermediario e avancado/JavaScript - DOM/js/03-script.js
@@ -0,0 +1,27 @@
+function salvar(){
+	
+//Button salvar informações do usuário, varias formas de fazer:
+
+		/*const obj=document.getElementsByName("cadastro")
+		console.log(obj)
+
+		const obj1 = document.getElementById('nome')
+		console.log(obj1.value)*/
+
+		/*console.log(document.cadastro.nome)
+		console.log(document.cadastro.sexo)*/
+
+		document.cadastro.nome.value = "leandro"
+		document.cadastro.sexo.value = "m"
+		
+		}
+
+		
+		/*const objeto=document.getElementsByName('cadastro')
+		console.log(objeto)
+		console.log(document.forms)*/
+
+
+
+		// várias opções não estão funcionando nestas nosvas versões, essas acima estão ok
+	
