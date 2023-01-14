33902881f4255ea012834dba63c866bc41c3ce06
LEANDRO CF <83041805+leandrocf79@users.noreply.github.com>
2023-01-14 18:20:17 -0300

    Add files via upload

--- Aulas Full Stack WEB/Projeto13 -NODEJS_ ReactJS_ NextJS_ Storybook_ Strapi_ TypeScript e JavaScript/JavaScript voltado para React/02-criando-funcoes/script.js
+++ Aulas Full Stack WEB/Projeto13 -NODEJS_ ReactJS_ NextJS_ Storybook_ Strapi_ TypeScript e JavaScript/JavaScript voltado para React/02-criando-funcoes/script.js
@@ -1,8 +1,40 @@
 
 
-//FUNÇÃO ENTRAR
+//FUNÇÃO ENTRAR ( 1º método)
 
 function entrar(){
 	var area = document.getElementById('area');
-	var texto = prompt('Digite seu nome: '):
+	var texto = prompt('Digite seu nome: ');
+
+//Quando usuário não inserir informação
+	if(texto == '' || texto == null){
+		alert('Não pode deixar em branco.');
+		area.innerHTML = 'Sejam bem vindos... ';
+
+	}else{
+		area.innerHTML = "Bem vindo " + texto;
+	}
+}
+
+
+//FUNÇÃO ENTRAR ( 2º método) Nome e sobrenome
+
+function entrar2(){
+	var area = document.getElementById('area');
+
+//nome
+	var nome = prompt('Digite seu 1º nome: ');
+//sobrenome	
+	var sobrenome = prompt('Digite seu sobrenome: ');
+//idade	
+	var idade = prompt('Digite sua idade: ');
+
+//Quando usuário não inserir informação
+	if(nome == '' || nome == null || sobrenome == '' || sobrenome == null || idade == '' || idade == null){
+		alert('Responda todas as perguntas. Tente novamente...');
+		area.innerHTML = 'Sejam bem vindos... ';
+		
+	}else{
+		area.innerHTML = 'Bem vindo '+nome+' '+sobrenome+', idade '+idade+' anos.';
+	}
 }
