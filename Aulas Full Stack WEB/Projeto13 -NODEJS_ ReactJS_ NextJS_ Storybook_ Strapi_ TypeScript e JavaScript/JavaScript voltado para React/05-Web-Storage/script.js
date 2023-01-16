d158ad4e3c7605a7d6beb6823117d3cadb866a5b
LEANDRO CF <83041805+leandrocf79@users.noreply.github.com>
2023-01-16 19:33:24 -0300

    Add files via upload

---
+++ Aulas Full Stack WEB/Projeto13 -NODEJS_ ReactJS_ NextJS_ Storybook_ Strapi_ TypeScript e JavaScript/JavaScript voltado para React/05-Web-Storage/script.js
@@ -0,0 +1,23 @@
+//var nome = localStorage.nome;
+//console.log(nome);
+
+
+var nome = '';
+
+
+if (typeof localStorage.nome == 'undefined'){
+	localStorage.nome = prompt("Digite seu nome: ");
+}
+
+nome = localStorage.nome;
+
+document.getElementById('nome').innerHTML = nome;
+
+
+//Remover o nome
+
+function removerNome(){
+	localStorage.removeItem('nome');
+	
+}
+
